a31eaf96527d0e25ce4c6ee29126f4a5baaf1bf6
isaibau <isaisabi02@gmail.com>
2025-02-08 20:31:24 -0600

    Login UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './views/Home';
+import Login from './components/Login';
+
 function App() {
 
   return (
     <>
-      <div>Hola</div>
+    <BrowserRouter>        
+      <Routes>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
+
     </>
   )
 }
